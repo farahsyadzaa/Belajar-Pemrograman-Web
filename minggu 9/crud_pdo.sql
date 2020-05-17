04f30ad621a7dcf027e3e71302217db3cde1db65
unknown <farahsyadzaa@gmail.com>
2020-05-17 23:30:17 +0700

    menambahkan folder minggu 9

---
+++ minggu 9/crud_pdo.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 17, 2020 at 05:09 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud_pdo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `username` char(20) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `Nama` varchar(100) NOT NULL,
+  `Hak` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`username`, `Password`, `Nama`, `Hak`) VALUES
+('farah', 'farah123', 'farahsyadza', 1),
+('syadza', 'syadza123', 'syadzamufidah', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`username`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ minggu 9/sekolah/menu.php
@@ -0,0 +1,19 @@
+ <div class="menu">
+ <ul>
+	<li><a href="index.php?p=home">Home</a></li>
+	<li><a href="index.php?p=sekolah">Sekolah</a></li>
+	<li><a></a></li>
+	<li><a></a></li>
+	<li><a></a></li>
+	<li><a></a></li>
+	<li><a></a></li>
+	<li><a></a></li>
+	<li><a></a></li>
+	<li><a></a></li>
+	<li><a></a></li>
+	<li><a></a></li>
+	<li><a></a></li>
+	<li><a></a></li>
+	<li><a href="logout.php?p=logout">logout</a></li>
+ </ul>
+ </div>
